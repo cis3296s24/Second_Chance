0cffe70aec69b46637092252f4e63998dd788b53
enesmance <143619406+enesmance@users.noreply.github.com>
2024-03-27 20:50:12 -0400

    Update base.py

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-
+from player import Player
+from platforms import Platform
 
 pygame.init()
 
@@ -9,11 +10,18 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-
 #color
 RED = (255, 0, 0)
 
-image = pygame.image.load('character.png')  # Replace "your_image_path.png" with the path to your image
+# Platforms
+platform_group = pygame.sprite.Group()
+platform_group.add(
+    Platform(100, 100),
+    Platform(200, 200),
+    Platform(300, 300),
+    Platform(400, 400),
+    Platform(500, 500)
+)
 
 #character properties
 rect_width = 50
@@ -21,9 +29,7 @@ rect_height = 50
 rect_x = (screen_width - rect_width) // 2
 rect_y = (screen_height - rect_height) // 2
 rect_speed = 5
-
-scale_factor = 2
-image = pygame.transform.scale(image, (rect_width * scale_factor, rect_height * scale_factor))  # Scale image to match rectangle size
+player = Player(rect_x, rect_y, rect_width, rect_height, RED, platform_group)
 
 #gravity properties
 gravity = 0.5
@@ -42,35 +48,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #get keys that are pressed
-    keys = pygame.key.get_pressed()
+    player.update()
 
-    #move as specified
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        rect_x -= rect_speed
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        rect_x += rect_speed
-
-    #jump
-    if keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_w]:
-        if rect_y == screen_height - rect_height:  #check if the character is on the ground
-            vertical_velocity = jump_strength
-
-    # Apply gravity
-    vertical_velocity += gravity
-    rect_y += vertical_velocity
-
-    #keep rect in screen
-    rect_x = max(0, min(screen_width - rect_width, rect_x))
-    rect_y = max(0, min(screen_height - rect_height, rect_y))
-
-    #draw rect (character in future)
-    pygame.draw.rect(screen, RED, (rect_x, rect_y, rect_width, rect_height))
-    
-    image_x = rect_x - (rect_width * (scale_factor - 1)) / 2
-    image_y = rect_y - (rect_height * (scale_factor - 1)) / 2
-    
-    screen.blit(image, (image_x, image_y))
+    platform_group.draw(screen)
+    player.draw()
 
     pygame.display.flip()
     clock.tick(60)
