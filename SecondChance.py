bccb73888a3eeb3eb59b8601c7ec66d254bd0e5a
MPags02 <30301995+MPags02@users.noreply.github.com>
2024-04-11 18:45:38 -0400

    get up to date with state changes

--- SecondChance.py
+++ SecondChance.py
@@ -14,7 +14,6 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
         self.username = "" 
-
         
         pg.display.set_caption("Second Chance")
         
@@ -27,8 +26,6 @@ class Game:
         setattr(State, "manager", self.manager)
 
     def run(self):
-        self.get_username() # get username before running game
-
         while self.running:
             events = pg.event.get()
             for event in events:
@@ -48,43 +45,6 @@ class Game:
         self.character_dir = os.path.join(self.assets_dir, "characters")
         self.background_dir = os.path.join(self.assets_dir, "backgrounds")
         self.resources_dir = os.path.join("resources")
-
-    def get_username(self):
-        # This method prompts the user for a username before starting the game
-        input_rect = pg.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 12, 200, 24)
-        font = pg.font.Font(None, 24)
-        active = False
-        
-        while True:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-                    sys.exit()
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    if input_rect.collidepoint(event.pos):
-                        active = not active
-                    else:
-                        active = False
-                if event.type == pg.KEYDOWN:
-                    if active:
-                        if event.key == pg.K_RETURN:
-                            return
-                        elif event.key == pg.K_BACKSPACE:
-                            self.username = self.username[:-1]
-                        else:
-                            self.username += event.unicode
-
-            self.screen.fill((30, 30, 30))
-            pg.draw.rect(self.screen, (255, 255, 255), input_rect, 2)
-            text_prompt = font.render("Enter your username: ", True, (255, 255, 255))
-            text_prompt_rect = text_prompt.get_rect(midbottom=input_rect.topleft)
-            self.screen.blit(text_prompt, text_prompt_rect)
-            # Render the username directly inside the input rectangle
-            text_surface = font.render(self.username, True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center=input_rect.center)
-            self.screen.blit(text_surface, text_rect)
-            pg.display.flip()
-
             
 if __name__ == "__main__":
     pg.init()
