7e965627200e831e4216aab0b9eb38018b6eb3ea
VictorTran259 <tun46575@temple.edu>
2024-04-17 17:41:43 -0400

    Added the reflex second chance minigame

--- src/states/minigames/__init__.py
+++ src/states/minigames/__init__.py
@@ -1,4 +1,5 @@
 from .reaction_time import ReactionTime
 from .memory import Memory
+from .reflexes import Reflexes
 
-__all__ = ["ReactionTime", "Memory"]
+__all__ = ["ReactionTime", "Memory", "Reflexes"]
