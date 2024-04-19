5296f478af67ed6b9e9512dba56ee75b469fca73
enesmance <143619406+enesmance@users.noreply.github.com>
2024-04-18 20:24:07 -0400

    fixed bug of minigame not working

--- src/states/minigames/__init__.py
+++ src/states/minigames/__init__.py
@@ -2,6 +2,7 @@ from .reaction_time import ReactionTime
 from .memory import Memory
 from .reflexes import Reflexes
 from .flying_green import FlyingGreen
-from .reaction_time import ReactionTime
+from .matching_game import Matching
+
 
 __all__ = ["ReactionTime", "Memory", "Reflexes", "FlyingGreen", "Matching"]
